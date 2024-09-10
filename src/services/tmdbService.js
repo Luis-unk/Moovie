38b5c396e976e8e9448c6dba7b79a67398b9d197
Luis-unk <santosluiss2022@gmail.com>
2024-09-10 19:58:50 -0300

    10/09/24 - first

--- src/services/tmdbService.js
+++ src/services/tmdbService.js
@@ -1,8 +1,41 @@
 import axios from "axios";
 
-const tmdb = axios.create({
-  baseURL:
-    "https://api.themoviedb.org/3/movie/157336?api_key=8fc5c85730d3b70ddeb9a3d47b0e5c83",
-});
+const TMDBUrl = axios.create({
+    baseURL: "https://api.themoviedb.org/3/movie/157336?",
+    searchURL: "https://api.themoviedb.org/3/search/movie?query"
+})
 
-export default tmdb;
+export const fetchFilms = async () => {
+    response = await axios.get(`https://api.themoviedb.org/3/movie/157336?api_key=8fc5c85730d3b70ddeb9a3d47b0e5c83`)
+
+    return response.data;
+}
+
+export const fetchFilmsById = async (movieId) => {
+    response = await axios.get(`${TMDBUrl}/${movieId}?api_key=${process.env.API_KEY}`)
+
+    return response.data;
+}
+
+export const fetchIdByTitle = async (title) => {
+    title = title.replace(/ /g, "+")
+
+    response = await axios.get(`${searchURL}=${title}&${process.env.API_KEY}`)
+
+    return response.data.id;
+}
+
+/*
+FAZER REQUISIÇÕES USANDO APPEND_TO_RESPONSE -> FAZ DUAS OU MAIS REQUISIÇÕES JUNTAS
+
+exemplo: 
+https://api.themoviedb.org/3/movie/157336?api_key=8fc5c85730d3b70ddeb9a3d47b0e5c83&append_to_response=videos
+
+https://api.themoviedb.org/3/movie/157336?api_key=8fc5c85730d3b70ddeb9a3d47b0e5c83&append_to_response=videos,images
+
+maneira separada: 
+
+https://api.themoviedb.org/3/movie/157336?api_key=8fc5c85730d3b70ddeb9a3d47b0e5c83
+https://api.themoviedb.org/3/movie/157336/videos?api_key=8fc5c85730d3b70ddeb9a3d47b0e5c83
+
+*/
